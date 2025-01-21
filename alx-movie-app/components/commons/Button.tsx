60cf3fdae95c6248aab825dcfad8c6d4ec76b5ae
Adunolami <abinatitilayo2@gmail.com>
2025-01-21 12:50:46 +0100

    update

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,3 +1,12 @@
+// Example: Button.tsx
 import React from 'react';
 
-const 
+const Button: React.FC = () => {
+  return (
+    <button className="px-4 py-2 bg-blue-500 text-white rounded">
+      Click me
+    </button>
+  );
+};
+
+export default Button;
